7a4b2f0c1e0664a0877d210b023864f75819aae7
Kirill Shmakov <shmakir@yandex.ru>
2025-12-03 08:44:10 +0100

    E26: Better logging

--- src/inspector/v8-debugger.cc
+++ src/inspector/v8-debugger.cc
@@ -583,13 +583,12 @@ class ThreadStateManager {
       call_function->Call(v8_context, v8_context->Global(), num_args, argv);
 
     task = TopTaskFor(t_worker_thread_id);
+    task->result = V8SerializeResult(isolate, call_result.ToLocalChecked());
 
     if (try_catch.HasCaught() || call_result.IsEmpty()) {
       v8::String::Utf8Value msg(isolate, try_catch.Exception());
       LogV8("ProcessTaskOnStack.3/3 [failed with exception]", "msg", *msg ? *msg : "<unknown>", "call_id", task->call_id);
-      task->result = V8SerializeResult(isolate, call_result.ToLocalChecked());
     } else {
-      task->result = V8SerializeResult(isolate, call_result.ToLocalChecked());
       LogV8("ProcessTaskOnStack.3/3", "type", static_cast<int>(task->result->commTypeID), "call_id", task->call_id);
     }
 
